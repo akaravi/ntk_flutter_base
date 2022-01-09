02f855ff4c587f72a1dd7c407d7ba3859f831fb9
ixiflutter1313 <mj.parishani@outlook.com>
2022-01-09 19:04:21 +0330

    f

---
+++ lib/src/services/NewsModelApi.dart
@@ -0,0 +1,17 @@
+
+import 'package:base/src/models/entityModel/base/ErrorException.dart';
+import 'package:base/src/models/entityModel/base/FilterModel.dart';
+import 'package:base/src/models/entityModel/news/NewsContentModel.dart';
+import 'package:base/src/services/baseService.dart';
+
+class NewsModelApi extends BaseService<int,NewsContentModel>{
+  NewsModelApi(String controllerURL, String baseUrl) : super(controllerURL, baseUrl);
+
+  // @override
+  // Future<ErrorException<NewsContentModel>> createTask(String fullPath, FilterModel filter) {
+  //  return super.createTask(fullPath, filter);
+  // }
+
+
+
+}
--- lib/src/services/baseService.dart
+++ lib/src/services/baseService.dart
@@ -1,3 +1,14 @@
-class BaseService {
-  String? baseURL;
+import 'package:base/src/models/entityModel/base/ErrorException.dart';
+import 'package:base/src/models/entityModel/base/FilterModel.dart';
+import 'package:base/src/models/entityModel/news/NewsContentModel.dart';
+import 'package:base/src/services/ApiMethod.dart';
+import 'package:retrofit/retrofit.dart';
+
+abstract class BaseService<INPUT, OUTPUT>  {
+  String controllerURL;
+  String baseUrl;
+
+  BaseService(this.controllerURL, this.baseUrl);
+
+
 }
