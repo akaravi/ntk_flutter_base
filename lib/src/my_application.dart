246bed06c3b4d2dd8983157412d6899424d6a201
ixiflutter1313 <mj.parishani@outlook.com>
2022-01-11 00:54:42 +0330

    add saved

--- lib/src/my_application.dart
+++ lib/src/my_application.dart
@@ -1,36 +1,92 @@
+import 'package:base/src/backend/config/my_application_prefnrece.dart';
+
 class MyApplication {
   static MyApplication? _instance;
 
-  String deviceName = "sony Z";
+  String _deviceName = "sony Z";
 
-  var notificationID = "";
+  var _notificationID = "";
 
   factory MyApplication.get() => _instance ??= MyApplication._();
 
   MyApplication._();
 
   //token of app
-  String token = "";
+  String _token = "";
 
   //base url of apis
-  String baseUrl = 'https://apicms.ir/';
+  String _baseUrl = 'https://apicms.ir/';
 
-  String packageName = 'ntk.android.ticketing.APPNTK';
+  String _packageName = 'ntk.android.ticketing.APPNTK';
 
   //locale
-  String lang = "fa";
+  String _lang = "fa";
 
   //country of user
-  String country = "IR";
+  String _country = "IR";
 
   //sim operator name
-  String simOperator = "MTN";
+  String _simOperator = "MTN";
 
   //as "AppBuildVer" in headers for version of app
-  int versionCode = 1;
+  int _versionCode = 1;
 
   //as "AppSourceVer" in headers for version sourced used
-  String versionName = '1.2.22';
+  String _versionName = '1.2.22';
 
-  String deviceId = "SONY_Z_DEVICE_ID";
+  String _deviceId = "SONY_Z_DEVICE_ID";
+
+  String get deviceName => _deviceName;
+
+  String get deviceId => _deviceId;
+
+  String get versionName => _versionName;
+
+  int get versionCode => _versionCode;
+
+  String get simOperator => _simOperator;
+
+  String get country => _country;
+
+  String get lang => _lang;
+
+  String get packageName => _packageName;
+
+  String get baseUrl => _baseUrl;
+
+  String get token => _token;
+
+  get notificationID => _notificationID;
+}
+
+class ApplicationChangeNotifier {
+  ApplicationChangeNotifier(MyApplicationPreference s);
+
+  setToken(String s) {
+    MyApplication.get()._token = s;
+  }
+
+  void setLanguage(String s) {
+    MyApplication.get()._lang = s;
+  }
+
+  void setCountry(String s) {
+    MyApplication.get()._country = s;
+  }
+
+  void simOperator(String s) {
+    MyApplication.get()._simOperator = s;
+  }
+
+  void setSimOperator(String s) {
+    MyApplication.get()._simOperator = s;
+  }
+
+  void setDeviceId(String s) {
+    MyApplication.get()._deviceId = s;
+  }
+
+  void setDeviceName(String s) {
+    MyApplication.get()._deviceName = s;
+  }
 }
