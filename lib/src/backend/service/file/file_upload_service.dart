dd80789cb0175a626916d67a8431dd007f9874b8
MJ.Dell <javadparishani@gmail.com>
2023-04-27 03:27:40 +0430

    Update file_upload_service.dart

--- lib/src/backend/service/file/file_upload_service.dart
+++ lib/src/backend/service/file/file_upload_service.dart
@@ -2,15 +2,27 @@ import 'dart:io';
 
 import 'package:base/src/backend/api/file/file_upload_api.dart';
 import 'package:base/src/backend/config/dio.dart';
+import 'package:base/src/models/entity/base/error_exception.dart';
+import 'package:base/src/models/entity/file/file_upload_model.dart';
+import 'package:base/src/ntk_application.dart';
+
 
 class FileUploadService extends DioApi {
   //api caller reference
   late FileUploadApi directAPI;
 
   FileUploadService() {
-    directAPI = FileUploadApi.create(jsonDecodeDio());
+    directAPI = FileUploadApi.create(jsonDecodeDio(),
+        baseUrl: NTKApplication.get().uploadUrl);
   }
-  upload(File f){
-    return directAPI.uploadFileWithPartMap("fname", f);
+
+  Future<FileUploadModel?> upload(File f) async {
+    ErrorException<FileUploadModel> res =
+        await directAPI.uploadFileWithPartMap("fname", f);
+    if (res.isSuccess) {
+      return res.item;
+    } else {
+      return null;
+    }
   }
 }
